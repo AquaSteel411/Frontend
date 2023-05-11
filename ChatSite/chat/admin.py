0ad6402d3a251b1a747fbb2e62cd74711c1625ea
AquaSteel411 <Popov_d1994@mail.ru>
2023-05-11 21:09:20 +0700

    Delete ChatSite directory

--- ChatSite/chat/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import ChatMessage, CustomRoom, UserChat
-
-
-admin.site.register(ChatMessage)
-admin.site.register(CustomRoom)
-admin.site.register(UserChat)
