83efc9b2984fa4a59ad25fcdb925dcd88e7bb512
AquaSteel411 <Popov_d1994@mail.ru>
2023-05-11 21:11:04 +0700

    Completed E 6.9

---
+++ ChatSite/accounts/views.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.models import User
+from django.views.generic.edit import CreateView
+
+
+
+
+class SignUp(CreateView):
+    model = User
+    success_url = '/accounts/login'
+    template_name = 'registration/signup.html'
